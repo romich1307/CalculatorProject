44b9f4ce42da74f3f1044e3c8e9bc96f90a63fcf
romich1307 <rcamargoh@unsa.edu.pe>
2025-03-05 12:22:10 -0500

    Corrección

--- Calculator.java
+++ Calculator.java
@@ -5,23 +5,23 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Bienvenido a la calculadora");
-        System.out.print("Ingrese el primer numero: ");
+        System.out.print("Ingrese el primer número: ");
         int primerNumero = sc.nextInt();
-        System.out.print("Ingrese el segundo numero: ");
+        System.out.print("Ingrese el segundo número: ");
         int segundoNumero = sc.nextInt();
         
         System.out.println("\nLos resultados son:");
         int suma = add(primerNumero, segundoNumero);
         int resta = sub(primerNumero, segundoNumero);
         int multiplicacion = mul(primerNumero, segundoNumero);
-        double division =  div(primerNumero, segundoNumero) ; 
-        int modulo =  mod(primerNumero, segundoNumero) ;
+        double division = div(primerNumero, segundoNumero); 
+        int modulo = mod(primerNumero, segundoNumero);
         
         System.out.println("La suma es: " + suma);
         System.out.println("La resta es: " + resta);
-        System.out.println("La multiplicacion es: " + multiplicacion);
-        System.out.println("La division es: " +  division );
-        System.out.println("El modulo es: " + modulo );
+        System.out.println("La multiplicación es: " + multiplicacion);
+        System.out.println("La división es: " + division);
+        System.out.println("El módulo es: " + modulo);
         
         sc.close();
     }
@@ -29,6 +29,27 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static double div(int a, int b) {
+        if (b == 0) {
+            System.out.println("ERROR: División entre cero no permitida");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+}
 
     public static int sub(int a, int b) {
         return a - b;
