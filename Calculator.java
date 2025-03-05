df844924e1612e8be8e41a5eef6a4281aa73479c
romich1307 <rcamargoh@unsa.edu.pe>
2025-03-05 12:06:52 -0500

    Corrección de división y manejo de división entre cero

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,22 @@
 public class Calculator{
 	public int add(int a,int b){
-	return a+b;
+		return a+b;
 	} 
 
 	public int sub(int a, int b) {
-        return a - b;
+        	return a - b;
     	}
 
 	public int mul(int a, int b) {
-        return a * b;
+        	return a * b;
     	}
+
 	public double div (int a, int b) {
-		return a / b;
+		if(b==0){
+			System.out.println("ERROR:División entre cero no permitida");
+			return 0;
 		}
+		return (double) a / b;
+	}
 	
 }
