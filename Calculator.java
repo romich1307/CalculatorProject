8e86024e3203ec1e3fa9f1a25f54d6d501aff9f6
romich1307 <rcamargoh@unsa.edu.pe>
2025-03-05 11:38:44 -0500

    Añadí operaciones sub y mul

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,13 @@
 public class Calculator{
 	public int add(int a,int b){
 	return a+b;
-	}
+	} 
+
+	public int sub(int a, int b) {
+        return a - b;
+    	}
+
+	public int mul(int a, int b) {
+        return a * b;
+    	}
 }
